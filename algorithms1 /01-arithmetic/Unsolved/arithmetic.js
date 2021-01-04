ec65dbf6e1c0bf6d76ca6a9d320a95f9dd801067
Mateo Ferreira <matteo948@icloud.com>
2021-01-04 00:04:41 -0500

    first commit

---
+++ algorithms1 /01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,19 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) { num1 + num2
+    return add 
+};
+
+
+
+var subtract = function(num1, num2) {
+    num1 - num2
+    return subtract
+};
+
+
+var multiply = function(num1, num2) {
+    
+};
+
+var divide = function(num1, num2) {};
